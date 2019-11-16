3768899ddd15a4287a2d1d1613d54196bd3df6ef
irksomecadet <sudeepsgeorge@gmail.com>
2019-11-16 02:57:28 -0500

    Spam classification using SVM algorithm

---
+++ ex6/ex6.m
@@ -0,0 +1,43 @@
+clear ; close all; clc
+fprintf('Loading and Visualizing Data ...\n')
+load('ex6data1.mat');
+plotData(X, y);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+load('ex6data1.mat');
+fprintf('\nTraining Linear SVM ...\n')
+C = 1000;
+model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+visualizeBoundaryLinear(X, y, model);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+fprintf('\nEvaluating the Gaussian Kernel ...\n')
+x1 = [1 2 1]; x2 = [0 4 -1]; sigma = 2;
+sim = gaussianKernel(x1, x2, sigma);
+fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = %f :' ...
+         '\n\t%f\n(for sigma = 2, this value should be about 0.324652)\n'], sigma, sim);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+fprintf('Loading and Visualizing Data ...\n')
+load('ex6data2.mat');
+plotData(X, y);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+fprintf('\nTraining SVM with RBF Kernel (this may take 1 to 2 minutes) ...\n');
+load('ex6data2.mat');
+C = 1; sigma = 0.1;
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+visualizeBoundary(X, y, model);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+fprintf('Loading and Visualizing Data ...\n')
+load('ex6data3.mat');
+plotData(X, y);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
+load('ex6data3.mat');
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
+fprintf('Program paused. Press enter to continue.\n');
+kbhit()
